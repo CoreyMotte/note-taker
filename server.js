7dbab52edc43328653fe5690d5bbd0c5e16d49e0
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-04-12 17:23:06 -0400

    added express.static for index.html and css, and added sendFile for the json data

--- server.js
+++ server.js
@@ -2,15 +2,39 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+let noteList = require('./db/db.json');
+
 const app = express();
 const port = process.env.PORT || 8001;
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+class Note {
+    constructor(title, text) {
+        Note.lastId++;
+        this.id = Note.lastId;
+        this.title = title;
+        this.text = text;
+    }
+}
+
+Note.lastId = 0;
 
 app.get('/notes', function(req, res) {
-    res.sendFile(path.join(__dirname, 'public/notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', function(req, res) {
+    res.sendFile(path.join(__dirname, 'db/db.json'))
+});
+
+app.post('/api/notes', function(req, res) {  
+    let note = new Note(req.body.title, req.body.text);
+    noteList.push(note);
+    fs.writeFile(__dirname + '/db/db.json', JSON.stringify(noteList), err => {});
+    res.json(note);
 });
 
 app.listen(port);
